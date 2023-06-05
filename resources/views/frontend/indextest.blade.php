294573b560239298436175790ab5bde886b7fe8e
Suryaprakash Tiwari <suryaprakashtiwari01@gmail.com>
2023-06-06 03:09:47 +0530

    Console Error Resolved

--- resources/views/frontend/indextest.blade.php
+++ resources/views/frontend/indextest.blade.php
@@ -1430,7 +1430,7 @@ input[type="checkbox"]{
           document.getElementById("days").innerText = Math.floor(distance / (day)),
             document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            //document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
    
           //do something later when date is reached
           if (distance < 0) {
