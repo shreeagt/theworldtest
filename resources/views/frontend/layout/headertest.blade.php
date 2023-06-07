3c5966df59085a1fc42340a5c84e7f9514ca594e
sachhidanandagt <guptasachidananad.agt@gmail.com>
2023-06-07 12:25:07 +0530

    changes logo

--- resources/views/frontend/layout/headertest.blade.php
+++ resources/views/frontend/layout/headertest.blade.php
@@ -53,7 +53,7 @@
 			<div class="container-fluid relative">
 				<a class="navbar-brand js-scroll-trigger" href="/#page-top">
 					{{-- <h2 class="white">WTC</h2> --}}
-					 <img src="/img/logo.png" alt="Logo" class="img-fluid center-block logo" /> 
+					 <img src="/img/logo.svg" alt="Logo" class="img-fluid center-block logo" /> 
 				</a>
 				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 					<i class="fa fa-bars" aria-hidden="true"></i>
