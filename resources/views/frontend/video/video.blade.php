715b2113828d42e60f5364322812d3255ea2e9d6
sachhidanandagt <guptasachidananad.agt@gmail.com>
2023-06-07 11:54:42 +0530

    add background color

--- resources/views/frontend/video/video.blade.php
+++ resources/views/frontend/video/video.blade.php
@@ -88,7 +88,7 @@
 <div class="modal fade" id="exampleModal{{ $video->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
-                <div class="modal-header" style="background:#f37224;">
+                <div class="modal-header" style="background:#817464;">
                     <h5 class="modal-title text-white" id="exampleModalLabel">{{ $video->description }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
